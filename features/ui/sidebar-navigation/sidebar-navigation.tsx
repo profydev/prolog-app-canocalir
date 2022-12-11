1b79c00c4c5b72c11842682ba5d2f0509b3c611a
Can Berk Ocalir <can.ocalir@gmail.com>
2022-12-11 17:54:49 +0300

    Support mailto created

--- features/ui/sidebar-navigation/sidebar-navigation.tsx
+++ features/ui/sidebar-navigation/sidebar-navigation.tsx
@@ -193,7 +193,10 @@ export function SidebarNavigation() {
               text="Support"
               iconSrc="/icons/support.svg"
               isCollapsed={isSidebarCollapsed}
-              onClick={() => alert("Support")}
+              onClick={() =>
+                (window.location.href =
+                  "mailto:support@prolog-app.com?subject=Support Request:")
+              }
             />
             <CollapseMenuItem
               text="Collapse"
